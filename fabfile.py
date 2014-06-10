b3f811985dc1d2ee7f06dbeb14d6842783db8499
Obulapathi N Challa <obulpathi@gmail.com>
2014-06-10 14:29:21 +0000

    updaet website

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
+import os
 import getpass
 
 from fabric.api import env, run, parallel
+from jinja2 import Environment, FileSystemLoader
 
 
 hosts = ['root@benchmarks-dfw', 'root@benchmarks-ord', 'root@benchmarks-iad',
@@ -30,7 +32,8 @@ def _benchmark(tenant_id, auth_token):
     run("REGION=%s TENANT_ID=%s AUTH_TOKEN=%s bash /root/csi-marconi/benchmark.sh" %
         (region, tenant_id, auth_token))
     # copy the logs dir to local log server dir
-    get("/root/.tsung/log/*", "/root/logs/")
+    get("/root/.tsung/log/*", "/root/logs/" + region)
+    get("/root/.tsung/log/*", "/usr/share/nginix/html/logs/" + region)
 
 
 def benchmark():
@@ -38,3 +41,21 @@ def benchmark():
     auth_token = getpass.getpass('Enter auth_token: ')
     env.host = hosts
     _benchmark(tenant_id, auth_token)
+
+# update webpages
+def update_website():
+    logs_path = "/usr/share/nginx/html/logs/"
+    env = Environment(loader=FileSystemLoader('website/'))
+    template = env.get_template('dc.html')
+    datacenters = os.listdir(logs_path)
+    for datacenter in datacenters:
+        benchmarks = os.listdir(logs_path + datacenter)
+        latest_benchmarks = sorted(benchmarks)[:10]
+        rendered_template = template.render(datacenter = datacenter, benchmarks = benchmarks)
+        dc_html = "/root/website/%s.html" % datacenter
+        with open(dc_html, "w") as fh:
+            fh.write(rendered_template)
+        # remove the outdates logs
+        outdated_benchmarks = benchmarks[10:]
+        for benchmark in outdate_benchmarks:
+            os.rmdir(logs_path + datacenter + "/" + benchmark)
--- update_website.py
+++ update_website.py
@@ -18,7 +18,14 @@ def update_website(logs_path):
     for datacenter in datacenters:
         benchmarks = os.listdir(logs_path + datacenter)
         latest_benchmarks = sorted(benchmarks)[:10]
-        rendered_template = template.render(datacenter, benchmarks)
+        rendered_template = template.render(datacenter = datacenter, benchmarks = benchmarks)
         dc_html = "/root/website/%s.html" % datacenter
-        with open(dc_html), "w") as fh:
+        with open(dc_html, "w") as fh:
             fh.write(rendered_template)
+        # remove the outdates logs
+        outdated_benchmarks = benchmarks[10:]
+        for benchmark in outdate_benchmarks:
+            os.rmdir(logs_path + datacenter + "/" + benchmark)
+
+if __name__ == "__main__":
+    update_website("/root/logs/")
